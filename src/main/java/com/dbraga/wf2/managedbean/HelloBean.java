4de2f385e9b14823f74857a084b3b962c4105c2d
Danilo Braga <dannaril@gmail.com>
2015-06-02 21:54:04 -0300

    Creation of the project JSF 2.2 + Maven + Eclipse + java 8

---
+++ src/main/java/com/dbraga/wf2/managedbean/HelloBean.java
@@ -0,0 +1,26 @@
+package com.dbraga.wf2.managedbean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+ 
+	private static final long serialVersionUID = 1L;
+ 
+	private String name;
+ 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void hello() {
+		System.out.println("Hello "+ name);
+	}
+}
